f018a203712326a059615f776629572f804480fd
Athreya211 <60138546+Athreya211@users.noreply.github.com>
2021-07-08 14:28:17 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,18 @@
+#add implementation
+def add(x,y):
+    return x+y
+    
+#subtraction implementation    
+def subtract(x,y):
+    return x-y
+    
+#multiplication implementation    
+def multiply(x,y):
+    return x*y
+    
+#division implementation    
+def divide(x,y):
+    if y==0:
+        return DIVIDE_BY_0_ERROR
+    else:
+        return x/y
